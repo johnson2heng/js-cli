52c13b0f8821f78718a88e20e9c11aa7c52197fa
jshnsonZheng <344871874@qq.com>
2019-09-04 11:45:10 +0800

    将babel更新到7.0版本
    为了能支持更多的js新特性（包括计划中的），故将babel版本更新到7.0+来实现新特性的支持

--- app/main.js
+++ app/main.js
@@ -1,1 +1,11 @@
-const obj = {};
+require('./index.less')
+
+const a = {
+    b: {
+        c: 1
+    }
+};
+
+const c = a?.b?.c;
+
+console.log(c);
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    plugins: [
-        require('autoprefixer')({
-            browsers: ['last 5 version',"> 1%"] //前五种浏览器版本
-        })
-    ]
-};
+    plugins: {
+        'autoprefixer': {}
+    }
+}
